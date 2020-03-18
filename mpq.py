520f081570c102a1813d540dd5c3a213cccd8e85
Askhat Molkenov <asmo@asmombp.local>
2020-03-18 15:49:58 +0600

    --run fix

--- mpq.py
+++ mpq.py
@@ -8,7 +8,7 @@ import datetime
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dir", default=".", required=False)
-parser.add_argument("run", action="store_true")
+parser.add_argument("--run", action="store_true")
 args = parser.parse_args()
 DIR = args.dir
 MODE = args.run
